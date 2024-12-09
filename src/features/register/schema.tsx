8f1778dbee8d62249384bb28329d2e02fc85e5c7
mishbahulmunir96 <157870568+mishbahulmunir96@users.noreply.github.com>
2024-12-09 22:15:14 +0700

    feat: registerpage (#5)

---
+++ src/features/register/schema.tsx
@@ -0,0 +1,20 @@
+import * as Yup from "yup";
+import YupPassword from "yup-password";
+
+YupPassword(Yup);
+
+export const RegisterSchema = Yup.object({
+  firstName: Yup.string().required("First name is required"),
+  phoneNumber: Yup.string().required("Phone Number is required"),
+  email: Yup.string().required("Email is required").email(),
+  password: Yup.string()
+    .required("Password is required")
+    .min(6)
+    .minLowercase(1)
+    .minUppercase(1)
+    .minNumbers(1)
+    .minSymbols(1),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password")], "Password must match")
+    .required("Confirm Password is required"),
+});
