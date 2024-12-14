1b254786e8eec238a6109546f8bc91f8a61caca4
mishbahulmunir96 <157870568+mishbahulmunir96@users.noreply.github.com>
2024-12-14 11:33:22 +0700

    Forgot password (#7)
    * fix: handling error on registerpage (toast email & phone number input)
    
    * feat: forgot password
    
    * feat: reset password

--- src/features/login/index.tsx
+++ src/features/login/index.tsx
@@ -29,10 +29,6 @@ const LoginPage = () => {
     <div className="px-2">
       <div className="container mx-auto mt-4 flex w-full justify-center">
         <Card className="w-[450px] p-6">
-          <div className="mb-6 w-auto rounded-md bg-blue-900 px-4 pb-4 pt-8">
-            <Logo />
-          </div>
-
           <form onSubmit={formik.handleSubmit}>
             <h1 className="mb-6 text-4xl font-extrabold text-slate-600">
               Sign in to <span className="font-bold text-blue-900">MAKÉT</span>
@@ -86,7 +82,7 @@ const LoginPage = () => {
               </div>
 
               <div className="">
-                <p>Forgot Password</p>
+                <Link href="/forgot-password">Forgot Password</Link>
               </div>
             </div>
 
